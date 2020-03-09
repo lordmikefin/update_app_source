06383121ab967224915cad23b43d38c7514b5c08
Lord Mike <lordmike@iki.fi>
2020-03-09 13:03:42 +0200

    Initialized the module

---
+++ update_app_source/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: UTF-8 -*-
+"""
+    update_app_source
+    ~~~~~~~~~~~~~~~~~
+
+    Module for automatically update content of 'app_source' project.
+
+    License of this script file:
+       MIT License
+
+    License is available online:
+      https://github.com/lordmikefin/update_app_source/blob/master/LICENSE
+
+    Latest version of this script file:
+      https://github.com/lordmikefin/update_app_source/blob/master/update_app_source/__init__.py
+
+
+    :copyright: (c) 2020, Mikko Niemelä a.k.a. Lord Mike (lordmike@iki.fi)
+    :license: MIT License
+"""
+
+__license__ = "MIT License"
+__version__ = "0.0.1"
+__revision__ = "update_app_source (module)  v" + __version__ + " (2020-03-09)"
+
