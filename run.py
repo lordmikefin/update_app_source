b20f3e3e907854bfd48fc137930517961c7cabc3
Lord Mike <lordmike@iki.fi>
2020-03-15 18:44:33 +0200

    list plugins

--- run.py
+++ run.py
@@ -8,5 +8,7 @@ import update_app_source
 if __name__ == '__main__':
     print(update_app_source.__revision__)
     source_file = './app_source/app_source.xml'
+    # TODO: use 'update_app_source' project to create the source file
+    # NOTE: for now testing with the sample
     update_app_source.source.create_sample(source_file)
     update_app_source.source.parse(source_file)
